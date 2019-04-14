803ad9d2f5a01269fc7816618d9287b2aac2b5d5
Di <eglet91@gmail.com>
2019-04-14 22:06:36 +0300

    fixed some bugs

--- find.cpp
+++ find.cpp
@@ -16,6 +16,15 @@
 
 static const unsigned long BUF_SIZE = 4096;
 
+void substr(const std::string &str, char *arg, size_t from, size_t to) {
+    arg = new char[to - from + 1];
+    size_t index = 0;
+    for (size_t i = from; i < to; ++i) {
+        arg[index++] = str[i];
+    }
+    arg[index] = '\0';
+}
+
 int str_to_int(const std::string &str, size_t from, size_t to) {
     int res = 0;
     for (size_t i = from; i < to; ++i) {
@@ -24,6 +33,12 @@ int str_to_int(const std::string &str, size_t from, size_t to) {
         res += (str[i] - '0') * pow(10, str.size() - i - 1 - from);
     }
     return res;
+}
+
+void get_first_arg(const std::string & path, char *arg_first) {
+  size_t index = path.size() - 1;
+  for (; index != 0 && path[index] != '/'; --index) {}
+  substr(path, arg_first, !index ? 0 : index + 1, path.size());
 }
 
 struct linux_dirent {
@@ -72,20 +87,22 @@ public:
                 break;
             }
             default: {
-                //error, it musn't happen
+                //error, it never happens. NEVER.
                 return false;
             }
             }
         }
         return result;
     }
-    char * get_exe_path() const {
+    const char * get_exe_path() const {
         return path_to_exec;
     }
 
     int set_flags(int argc, char* argv[]) {
         for (size_t i = 2; i < static_cast<size_t>(argc); i += 2) {
-            size_t arg_name_hash = std::hash<std::string>{}(std::string(argv[i]));            
+            size_t arg_name_hash = std::hash<std::string>{}(std::string(argv[i]));
+            if (argv[i +1] == nullptr)
+                return -1;
             std::string arg(argv[i + 1]);
             if (arg_name_hash == flags_hashes[0]) {
                 INUM = true;
@@ -128,7 +145,13 @@ public:
                         break;
                     }
                     default: {
-                        std::cerr << "Invalid size. Usage: -size n[ckGM]\n";
+                        std::cerr << "Invalid size. Usage: -size n[ckGM]\n"
+                                                    "File uses n units of space, rounding up.  The following suffixes can be used:\n"
+                                                    "`b'    for 512-byte blocks (this is the default if no suffix is used)\n"
+                                                    "`c'    for bytes\n"
+                                                    "`K'    for Kibibytes (KiB, units of 1024 bytes)\n"
+                                                   " `M'    for Mebibytes (MiB, units of 1024 * 1024 = 1048576 bytes)\n"
+                                                   " `G'    for Gibibytes (GiB, units of 1024 * 1024 * 1024 = 1073741824 bytes)\n\n";
                         return -1;
                     }
                     }
@@ -166,22 +189,15 @@ private:
     };
 };
 
-void substr(const std::string &str, char *arg, size_t from, size_t to) {
-    arg = new char[to - from + 1];
-    size_t index = 0;
-    for (size_t i = from; i < to; ++i) {
-        arg[index++] = str[i];
+void clean(char **str, size_t size) {
+    for (size_t i = 0; i <= size; ++i) {
+        if (str[i] != nullptr)
+            delete [] str[i];
     }
-    arg[index] = '\0';
+    delete [] str;
 }
 
-void get_first_arg(const std::string & path, char *arg_first) {
-  size_t index = path.size() - 1;
-  for (; index != 0 && path[index] != '/'; --index) {}
-  substr(path, arg_first, !index ? 0 : index + 1, path.size());
-}
-
-void execute_with_argv(const flags_wrapper &flags_wrp, const std::vector<std::string> &args_str) {
+int execute_with_argv(const flags_wrapper &flags_wrp, const std::vector<std::string> &args_str) {
     char **arguments = new char *[args_str.size() + 2];
 
     get_first_arg(flags_wrp.get_exe_path(), arguments[0]);
@@ -198,36 +214,29 @@ void execute_with_argv(const flags_wrapper &flags_wrp, const std::vector<std::st
 
     if (pid == 0) {
         int prog_stat = execvp(flags_wrp.get_exe_path(), arguments);
-        if (prog_stat < 0) {
-            std::cerr << "Can not execute " << flags_wrp.get_exe_path() << " \n";
-        }
+        std::cout << "Can't execute " << flags_wrp.get_exe_path() << ". " << strerror(errno) << "\n";
+        return prog_stat;
     } else if (pid == -1) {
-        std::cerr << "Can't create new process...\n";
+        std::cerr << "Can't create new process. " << strerror(errno) << "\n";
+        clean(arguments, args_str.size() + 2);
+        return -1;
     } else {
         int wstatus;
         if (waitpid(pid, &wstatus, WUNTRACED) == -1) {
-
-            std::cerr << "Error during execution program...\n";
-
+            std::cerr << "Error during execution program. " << strerror(errno) << '\n';
         } else {
             if (WIFEXITED(wstatus)) {
-                std::cout << "Execution " << flags_wrp.get_exe_path() << " terminated normally\n"
-                                                                         "Exit status is " << WEXITSTATUS(wstatus) << "\n" ;
+                std::cout << flags_wrp.get_exe_path() <<  ": exit code is " << WEXITSTATUS(wstatus) << "\n" ;
             } else {
                 std::cerr << "Error during termination of " << flags_wrp.get_exe_path() << " execution.\n";
             }
         }
-
-        for (size_t i = 0; i <= args_str.size(); ++i) {
-            if (arguments[i] != nullptr)
-                delete [] arguments[i];
-        }
-        delete [] arguments;
+        clean(arguments, args_str.size() + 2);
+        return 0;
     }
-
 }
 
-void run(const char *path, const flags_wrapper &flags_wrp){
+int run(const char *path, const flags_wrapper &flags_wrp){
     const char *this_dir = ".", *this_parent_dir = "..";
     bool exec = flags_wrp.isExec();
     std::queue<std::string> q;
@@ -245,13 +254,13 @@ void run(const char *path, const flags_wrapper &flags_wrp){
         int cur_fd = open(cur_path.c_str(), O_RDONLY, O_DIRECTORY);
 
         if (cur_fd == -1) {
-             std::cerr << "Can't open directory " << cur_path << ". \n";
+             std::cerr << "Can't open directory " << cur_path << ". \n" << strerror(errno) << "\n";
         } else {
             struct linux_dirent *dirent;
             while (true) {
                 int nread = syscall(SYS_getdents, cur_fd, buffer, BUF_SIZE);
                 if (nread == -1) {
-                    std::cerr << "Can't get entry of directory " << cur_path << "\n";
+                    std::cerr << "Can't get entry of directory " << cur_path << ". " << strerror(errno) << "\n";
                     break;
                 }
                 if (!nread) break;
@@ -274,7 +283,7 @@ void run(const char *path, const flags_wrapper &flags_wrp){
                     //For symlink "find" will get inode for this symlink, but not for file this symlink links to.
                     int stat_status = stat(child_path.c_str(), &statbuf);
                     if (stat_status == -1) {
-                        std::cerr << "Can't get information about file: " << child_path << ". \n";
+                        std::cerr << "Can't get information about file: " << child_path << ". " << strerror(errno) << "\n";
                     } else {
                         if (flags_wrp.satisfied(dirent->d_ino, dirent->d_name, statbuf.st_size, statbuf.st_nlink) && strcmp(this_parent_dir, dirent->d_name) && strcmp(this_dir, dirent->d_name)) {
                             if (!exec) {
@@ -287,18 +296,23 @@ void run(const char *path, const flags_wrapper &flags_wrp){
                     bpos += dirent->d_reclen;
                 }
             }
-            close(cur_fd);
+            if (close(cur_fd) == -1) {
+                std::cerr << cur_path << ": " << strerror(errno) << "\n";
+            }
         }
     }
 
     if (exec) {
-        execute_with_argv(flags_wrp, argv);
+        return execute_with_argv(flags_wrp, argv);
     }
+
+    return 0;
 }
 
 int main(int argc, char *argv[]) {
     if (argc < 2) {
-        std::cerr << "Incorrect input. Usage: ./find <path_to_dir>";
+        std::cerr << "Incorrect input. Usage: path_to_executable_file [args...]\n"
+                     "Supported arguments: [-name name] [-size [+=-]size[cbMKG]] [-inum inode] [-exec path_to_exe_file] [-nlinks nlinks]\n";
         return 0;
     }
     flags_wrapper flags;
@@ -311,8 +325,6 @@ int main(int argc, char *argv[]) {
         return 0;
     }
 
-    run( path, flags);
-
-    return 0;
+    return run( path, flags);
 }
 
